f715b76a539f5fed4fd575cd85d214396a6aa46f
swaandekkers <swaan.dekkers@gmail.com>
2015-11-10 12:02:30 +0100

    improved board for board visualisation

--- rushhour/test/test_board.py
+++ rushhour/test/test_board.py
@@ -2,7 +2,9 @@ from rushhour.board import Board
 from rushhour.car import Car
 from rushhour.position import Position
 
-board = Board(6, 6, [Car(Position(0, 0), True, 2)], Position(5, 2))
-test12 = board.is_winner() == False
+board1 = Board(6, 6, [Car(Position(0, 0), True, 2)], Position(5, 2))
+test12 = board1.is_winner() == False
 
 print("board can be no winner " + str(test12))
+
+board2 = Board(6, 6, [Car(Position(0, 0), True, 2)], Position(5, 2))
